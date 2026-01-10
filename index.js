eb063a5746eac151d3630f5400449dfa7d2c72af
Nicolas Fleitas <nicofleitaspy@gmail.com>
2026-01-10 09:01:25 -0300

    feat: agregar autenticación JWT
    - Implementar endpoint /login con generación de tokens
    - Crear middleware authenticateToken para verificación de tokens
    - Agregar endpoint de prueba /profile protegido con autenticación
    - Integrar jsonwebtoken para manejo de tokens JWT

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const { Pool, Connection } = require('pg')
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 const app = express()
 app.use(express.json())
@@ -42,6 +43,65 @@ app.post('/register', async (req, res) => {
         console.error(error);
         res.status(500).json({ error: "Error interno en el servidor" })
     }
+})
+// ---- ENDPOINT DE LOGIN ----
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+        const user = result.rows[0]
+
+        if (!user) {
+            return res.status(401).json({ error: "Credenciales inválidas" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password_hash)
+
+        if (!isMatch) {
+            return res.status(401).json({ error: "Credenciales inválidas" })
+        }
+
+        const payload = {
+            id: user.id,
+            email: user.email,
+            role: user.role
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' })
+
+        res.json({
+            message: "Login exitoso",
+            token: token
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Error en el servidor" })
+    }
+})
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).json({ error: "Acceso denegado. Token faltante" })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodeUser) => {
+        if (err) {
+            return res.status(403).json({ error: "Token inválido o expirado" })
+        }
+
+        // Si es válido, guarda los datos del usuario en el objeto (req)
+        // para que las siguiente funciones puedan usarlos
+        req.user = decodeUser
+        next() // Continuar al siguiente paso
+    })
+}
+
+app.get('/profile', authenticateToken, (req, res) => {
+    res.json({ message: `Bienvenido usuario ${req.user.email},`, data: req.user })
 })
 
 const PORT = 3000;
